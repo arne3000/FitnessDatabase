44055d261dcf5bb0129682423e0cd40ee1b045fb
Arne Ubelhor <ubelhor3000@gmail.com>
2019-02-24 23:16:55 +0000

    simplify

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+class ExerciseSummary {
+	public name: string;
+	public primaryMuscleGroup: string;
+
+	constructor(_name, _primaryMuscleGroup)
+	{
+		this.name = _name;
+		this.primaryMuscleGroup = _primaryMuscleGroup;
+	}
+}
+
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+	selector: 'app-home-page',
+	templateUrl: './home-page.component.html',
+	styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit 
+{
+  	public exercises: Array<ExerciseSummary>;
 
-  
+  	constructor() 
+  	{
+		this.exercises = new Array<ExerciseSummary>();
+  	} 
 
-  constructor() { }
-
-  ngOnInit() {
-
-  }
+  	ngOnInit() 
+  	{
+		this.exercises.push(new ExerciseSummary("push up", "back"));
+		this.exercises.push(new ExerciseSummary("push up", "back"));
+		this.exercises.push(new ExerciseSummary("push up", "back"));
+		this.exercises.push(new ExerciseSummary("push up", "back"));
+		this.exercises.push(new ExerciseSummary("push up", "back"));
+  	}
 
 }
